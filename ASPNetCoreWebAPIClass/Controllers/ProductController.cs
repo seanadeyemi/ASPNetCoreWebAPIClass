81e633b2d1c2e927871f415ddb081c2cbdeae698
seanadeyemi <seanadeyemi@gmail.com>
2023-11-25 17:32:29 +0100

    Add project files.

---
+++ ASPNetCoreWebAPIClass/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using ASPNetCoreWebAPIClass.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreWebAPIClass.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+
+        [HttpGet]
+        [Route("get-all-products")]
+        public IActionResult GetAllProducts()
+        {
+            List<ProdutResponse> products = new List<ProdutResponse>
+            {
+                new ProdutResponse { Id = 1, Name = "Jacket", Price = 20000M, Quantity = 5 },
+                new ProdutResponse { Id = 2, Name = "Wristwatch", Price = 25000M, Quantity = 5 },
+                new ProdutResponse { Id = 3, Name = "Phone", Price = 70000M, Quantity = 5 },
+                new ProdutResponse { Id = 4, Name = "TV", Price = 2600M, Quantity = 5 },
+                new ProdutResponse { Id = 5, Name = "Laptop", Price = 90000M, Quantity = 5 }
+            };
+
+
+            return Ok(products);
+
+        }
+
+    }
+}
---
+++ ASPNetCoreWebAPIClass/Models/ProdutResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ASPNetCoreWebAPIClass.Models
+{
+    public class ProdutResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
